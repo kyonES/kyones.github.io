160f22c6e18151f2a363d31fbe681c3d5a833942
nekomata <62155029+kyonES@users.noreply.github.com>
2022-03-24 16:47:01 +0900

    タイトルと自己紹介を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>ねこまたのページ</title>
+        <title>ねこまたの日記</title>
         <meta name="description" content="現在設定中" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>ねこまたの日記 </h1>
+      <h2>自己紹介</h2>
       ねこです。にゃーん。
       <h2>投稿一覧</h2>
       <ul>
