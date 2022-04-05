5e9f1ad121f561c254f28df9060083fb6f38c51b
nekomata <62155029+kyonES@users.noreply.github.com>
2022-04-05 13:32:31 +0900

    自己紹介とリンク追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,26 @@ export default function Home() {
       <h1>ねこまたの日記 </h1>
       <img src="/images/icon.jpg" height="200px"></img>
       <h2>自己紹介</h2>
-      ねこです。にゃーん。
+      ねこまたです。よろしくおねがいします。大学での主専攻である地球科学(物理)を勉強する傍、ゆるくPython、JavaScript、Haskellなどを触っています。
+      このサイトでは技術記事の他、趣味のアフタヌーンティーの記録を書き連ねていこうと思っています。
+      にゃーん。
+      <h2>リンク</h2>
+      <li>
+        github:{" "}
+        <a href="https://github.com/kyonES">https://github.com/kyonES</a>
+      </li>
+      <li>
+        Twitter:{" "}
+        <a href="https://twitter.com/what_is_pyoni">
+          https://twitter.com/what_is_pyoni
+        </a>
+      </li>
+      <li>
+        Instagram:{" "}
+        <a href="https://www.instagram.com/sub_neko">
+          https://www.instagram.com/sub_neko
+        </a>
+      </li>
       <h2>投稿一覧</h2>
       <ul>
         <li>
