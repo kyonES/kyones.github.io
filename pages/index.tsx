481eeb17fc8b394d9187eba374bf61dd703ce64e
nekomata <62155029+kyonES@users.noreply.github.com>
2022-04-14 22:03:16 +0900

    見出しにアイコンをつけた

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faPaw } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -13,13 +15,24 @@ export default function Home() {
         />
         <link rel="icon" href="/images/icon.jpg" />
       </Head>
-      <h1>ねこまたの日記 </h1>
+      <h1>
+        <FontAwesomeIcon icon={faPaw} size="xs" color="rgb(245, 109, 145)" />
+        ねこまたの日記{" "}
+      </h1>
       <img src="/images/icon.jpg" height="200px"></img>
-      <h2>自己紹介</h2>
+      <h2>
+        {" "}
+        <FontAwesomeIcon icon={faPaw} size="xs" />
+        自己紹介
+      </h2>
       ねこまたです。よろしくおねがいします。大学での主専攻である地球科学(物理)を勉強する傍、ゆるくPython、JavaScript、Haskellなどを触っています。
       このサイトでは技術記事の他、趣味のアフタヌーンティー、カフェ巡りの記録を書き連ねていこうと思っています。
       にゃーん。
-      <h2>リンク</h2>
+      <h2>
+        {" "}
+        <FontAwesomeIcon icon={faPaw} size="xs" />
+        リンク
+      </h2>
       <ul className="sample">
         <li>
           {" "}
@@ -44,7 +57,11 @@ export default function Home() {
           </a>
         </li>
       </ul>
-      <h2>投稿一覧</h2>
+      <h2>
+        {" "}
+        <FontAwesomeIcon icon={faPaw} size="xs" />
+        投稿一覧
+      </h2>
       <ul>
         <li>
           {
