ed2e4860bb144cb950e5e7b44bc399505bb9744d
nekomata <62155029+kyonES@users.noreply.github.com>
2022-05-20 20:13:36 +0900

    戻るボタン実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { AppProps } from "next/app";
+import Link from "next/link";
 config.autoAddCss = false;
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      {router.asPath.startsWith("/posts") ? (
+        <Link href="/">戻る(=^･ω･^=) </Link>
+      ) : null}
+    </>
+  );
 }
 export default MyApp;
